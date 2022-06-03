c98a4b24778a386a3623eb2de598cb188cfef1c9
nguynvanky <k.nguyn21@gmail.com>
2022-06-03 17:59:56 +0700

    a

--- config.js
+++ config.js
@@ -1,120 +1,27 @@
 var color_active = "#a4de02";
 
 function display_all() {
-    document.getElementById("click_fru").style.borderBottomColor = "white";
-    document.getElementById("click_nut").style.borderBottomColor = "white";
-    document.getElementById("click_veg").style.borderBottomColor = "white";
-
-    var elms = document.querySelectorAll("[id='fruits']");
-    for (var i = 0; i < elms.length; i++) {
-        if (elms[i].style.display == "none") elms[i].style.display = "block";
-    }
-    elms = document.querySelectorAll("[id='nuts']");
-    for (var i = 0; i < elms.length; i++) {
-        if (elms[i].style.display == "none") elms[i].style.display = "block";
-    }
-    elms = document.querySelectorAll("[id='vegetables']");
-    for (var i = 0; i < elms.length; i++) {
-        if (elms[i].style.display == "none") elms[i].style.display = "block";
-    }
+    document.getElementById("fru").style.borderBottomColor = "white";
+    document.getElementById("nut").style.borderBottomColor = "white";
+    document.getElementById("veg").style.borderBottomColor = "white";
 }
 
 function display_vegetables() {
-    var elms = document.querySelectorAll("[id='vegetables']");
-    document.getElementById("click_fru").style.borderBottomColor = "white";
-    document.getElementById("click_nut").style.borderBottomColor = "white";
-
-    var btn = document.getElementById("click_veg");
-    if (btn.style.borderBottomColor == color_active) {
-        elms = document.querySelectorAll("[id='fruits']");
-        btn.style.borderBottomColor = "white";
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-        elms = document.querySelectorAll("[id='nuts']");
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-    } else {
-        btn.style.borderBottomColor = color_active;
-        for (var i = 0; i < elms.length; i++) {
-            if ((elms[i].style.display = "none")) elms[i].style.display = "block";
-        }
-        var elements = document.querySelectorAll("[id='fruits']");
-        for (var i = 0; i < elements.length; i++) {
-            if ((elements[i].style.display = "block"))
-                elements[i].style.display = "none";
-        }
-        var elements_1 = document.querySelectorAll("[id='nuts']");
-        for (var i = 0; i < elements_1.length; i++) {
-            if ((elements_1[i].style.display = "block"))
-                elements_1[i].style.display = "none";
-        }
-    }
+    document.getElementById("fru").style.borderBottomColor = "white";
+    document.getElementById("nut").style.borderBottomColor = "white";
+    document.getElementById("veg".style.borderBottomColor = color_active);
 }
 
 function display_fruits() {
-    document.getElementById("click_veg").style.borderBottomColor = "white";
-    document.getElementById("click_nut").style.borderBottomColor = "white";
-    var elms = document.querySelectorAll("[id='fruits']");
-    var btn = document.getElementById("click_fru");
-    if (btn.style.borderBottomColor == color_active) {
-        btn.style.borderBottomColor = "white";
-        elms = document.querySelectorAll("[id='vegetables']");
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-        elms = document.querySelectorAll("[id='nuts']");
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-    } else {
-        btn.style.borderBottomColor = color_active;
-        for (var i = 0; i < elms.length; i++) {
-            if ((elms[i].style.display = "none")) elms[i].style.display = "block";
-        }
-        var elements = document.querySelectorAll("[id='vegetables']");
-        for (var i = 0; i < elements.length; i++) {
-            if ((elements[i].style.display = "block"))
-                elements[i].style.display = "none";
-        }
-        var elements_1 = document.querySelectorAll("[id='nuts']");
-        for (var i = 0; i < elements_1.length; i++) {
-            if ((elements_1[i].style.display = "block"))
-                elements_1[i].style.display = "none";
-        }
-    }
+    document.getElementById("veg").style.borderBottomColor = "white";
+    document.getElementById("nut").style.borderBottomColor = "white";
+    document.getElementById("fru".style.borderBottomColor = color_active);
+
 }
 
 function display_nuts() {
-    document.getElementById("click_fru").style.borderBottomColor = "white";
-    document.getElementById("click_veg").style.borderBottomColor = "white";
-    var elms = document.querySelectorAll("[id='nuts']");
-    var btn = document.getElementById("click_nut");
-    if (btn.style.borderBottomColor == color_active) {
-        btn.style.borderBottomColor = "white";
-        elms = document.querySelectorAll("[id='vegetables']");
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-        elms = document.querySelectorAll("[id='fruits']");
-        for (var i = 0; i < elms.length; i++) {
-            if (elms[i].style.display == "none") elms[i].style.display = "block";
-        }
-    } else {
-        btn.style.borderBottomColor = color_active;
-        for (var i = 0; i < elms.length; i++) {
-            if ((elms[i].style.display = "none")) elms[i].style.display = "block";
-        }
-        var elements = document.querySelectorAll("[id='vegetables']");
-        for (var i = 0; i < elements.length; i++) {
-            if ((elements[i].style.display = "block"))
-                elements[i].style.display = "none";
-        }
-        var elements_1 = document.querySelectorAll("[id='fruits']");
-        for (var i = 0; i < elements_1.length; i++) {
-            if ((elements_1[i].style.display = "block"))
-                elements_1[i].style.display = "none";
-        }
-    }
+    document.getElementById("fru").style.borderBottomColor = "white";
+    document.getElementById("veg").style.borderBottomColor = "white";
+    document.getElementById("nut".style.borderBottomColor = color_active);
+
 }
